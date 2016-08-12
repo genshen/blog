7c0fe9ef297df44718d16a099ee4e5f0f67ebc49
ChuGenshen <genshenchu@gmail.com>
2016-08-13 02:08:08 +0800

    add comment module

--- static/js/main.js
+++ static/js/main.js
@@ -2,6 +2,9 @@
  * Created by 根深 on 2016/8/11.
  */
 var settings = {};
+var NoAuthSnackBar = '<a data-dismiss="snackbar">Dismiss</a>' +
+    '<div class="snackbar-text">你需要登录认证后才能添加评论.' +
+    '(使用<a href="https://github.com" target="_blank">Github</a>账号登录)</div>';
 function init() {
     marked.setOptions({
         highlight: function (code) {
@@ -18,8 +21,17 @@ function init() {
                     template: '#list-template',
                     props: {
                         settings: Object,
-                        lists: Array,
                         show: Boolean
+                    },
+                    data: function () {
+                        return {
+                            lists: [{
+                                date: "3天前",
+                                title: "Hello World",
+                                summary: "Lorem ipsum dolor sit amet.Consectetur adipiscing elit.",
+                                img: "/static/img/brand.jpg"
+                            }]
+                        };
                     },
                     computed: {},
                     methods: {},
@@ -31,19 +43,64 @@ function init() {
                     template: '#detail-template',
                     props: {
                         settings: Object,
-                        lists: Array,
-                        show: Boolean
+                        show: Boolean,
+                        is_auth: Boolean //sync
                     },
                     data: function () {
-                        return {article: "# Marked in browser\n\nRendered by **marked**.\n```c\n int main(){\r\n if(i<o){j++;\r\nreturn 0}}\n```"}
+                        return {
+                            article: "# Marked in browser\n\nRendered by **marked**.\n```c\n int main(){\r\n if(i<o){j++;\r\nreturn 0}}\n```",
+                            comments: [
+                                {
+                                    user: {name: "he", url: "baidu.com", avatar: "/static/img/avatar.jpg"},
+                                    replies:[
+                                        {
+                                            user: {name: "he", url: "baidu.com", avatar: "/static/img/avatar.jpg"},
+                                            content: "Hello replies1",
+                                            date: "3天前"
+                                        },
+                                        {
+                                            user: {name: "he", url: "baidu.com", avatar: "/static/img/avatar.jpg"},
+                                            content: "Hello replies2",
+                                            date: "3天前"
+                                        }
+                                    ],
+                                    content: "Hello Comment",
+                                    date: "3天前",
+                                    new_reply:"",
+                                    show_reply_box:true
+                                },
+                                {
+                                    user: {name: "he", url: "baidu.com", avatar: "/static/img/avatar.jpg"},
+                                    replies:[
+
+                                    ],
+                                    content: "Hello Comment2",
+                                    date: "3天前"
+                                }
+                            ]
+                        }
                     },
                     computed: {},
                     filters: {
-                        marked: function(value){
+                        marked: function (value) {
                             return marked(value);
                         }
                     },
-                    methods: {},
+                    methods: {
+                        checkAuth: function () {
+                            if (this.is_auth) {
+                                return
+                            }
+                            $('#auth_model').modal('show')
+                        },
+                        submitComment: function () {
+                            if (!this.is_auth) {
+                                $("body").snackbar({alive: 4000, content: NoAuthSnackBar});
+                                return
+                            }
+                            console.log("here! ");
+                        }
+                    },
                     ready: function () {
 
                     }
@@ -55,16 +112,16 @@ function init() {
                 data: {
                     settings: settings,
                     title: "哈哈哈",
-                    list: {show: false, data: []},
-                    detail: {show: true, data: []}
+                    list: {show: false},
+                    detail: {show: true, is_auth: true}
                 },
                 ready: function () {
-                    this.list.data.push({
-                        date: "3天前",
-                        title: "Hello World",
-                        summary: "Lorem ipsum dolor sit amet.Consectetur adipiscing elit.",
-                        img: "/static/img/brand.jpg"
-                    });
+                },
+                methods: {
+                    openGithub: function () {
+                        window.open("https://github.com");
+                        $('#auth_model').modal('hide')
+                    }
                 }
             });
         });
