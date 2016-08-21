07860b062f7de3f10a933931cc62b9d0635d316c
ChuGenshen <genshenchu@gmail.com>
2016-08-22 01:38:37 +0800

    add posts model

--- models/posts.go
+++ models/posts.go
@@ -1,1 +1,15 @@
 package models
+
+import "gopkg.in/mgo.v2/bson"
+
+type Admin struct {
+	Id           bson.ObjectId `bson:"_id,omitempty"`
+	Title        string
+	Content      string
+	Summary      string
+	ViewCount    string `bson:"view_count"`
+	CommentCount string `bson:"comment_count"`
+	//Classify         string
+	//Tags         string
+	Status       int
+}
